e4010230b32415b248b0b09f1a682b2bb86294ef
ban <rayan170300@gmail.com>
2021-10-11 14:22:41 -0300

    new components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App'
+// import App from './App'
+import Home from './pages/Home'
+// import Repositories from './components/Repositories'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
